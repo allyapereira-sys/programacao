ff8ea9277953479cf71874386b638d5a12e5df1c
allyapereira-sys <allya_pereira@estudante.sesisenai.org.br>
2026-03-11 14:19:38 +0000

    update

--- Lista1 python/atividade18.py
+++ Lista1 python/atividade18.py
@@ -2,11 +2,11 @@ hora_normal = 10
 hora_extra = 15
 hora_normal = float(input("Digite a quantidade de horas trabalhadas"))
 hora_extra = float(input("Digite a quantidade de horas extras"))
+salário_bruto= (hora_normal*10)+(hora_extra*15)
+salario_liquido= salário_bruto-(salário_bruto*0.10)
+print("o salário bruto é",salário_bruto)
+print("o salário liquido é",salario_liquido)
 
-salário_bruto= (hora_normal+hora_extra)
-print(f" O salário bruto é:{salário_bruto}")*0.10
-salario_liquido= (hora_normal+hora_extra)*0.10
-print(f"o salário liquido é")
 
 
 
